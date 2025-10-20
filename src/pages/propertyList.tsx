166dde761c8f31784c08c5abeefcfffdb49bf5e5
Arya Singh <aryasixgh@gmail.com>
2025-10-20 10:52:47 +0530

    First commit

---
+++ src/pages/propertyList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const PropertyList = () => {
+  return (
+    <>
+      <div className="bg-white border-1 border-gray-200 rounded-2xl h-auto w-100 p-3">
+        <div className="bg-gray-200 rounded-2xl h-50"></div>
+        <div className="text-left text-gray-800 w-10/12">
+          <div>Views</div>
+          <div className="text-red-300 mt-2">Independent House</div>
+          <div className="font-semibold my-1">Godrej Society</div>
+          <div className="">
+            R.A. Kidwai Marg, Azad Nagar, Wadala, Mumbai, Maharashtra India
+          </div>
+          <div className="flex flex-row mt-2 font-medium">
+            <div className="mr-10">Beds: 2</div>
+            <div>Baths: 4</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PropertyList;
